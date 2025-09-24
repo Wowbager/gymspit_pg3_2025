509d4e5bcab3c38ed59c98dab7d2bfd45fb6f8cb
Ben Michael Dědič <ben.m.dedic@gmail.com>
2025-09-24 09:19:19 +0200

    fizzbuzz

---
+++ lectr_2/lectr_2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+string fizzBuzz(uint number)
+{
+    string output = "";
+
+    if (number % 3 == 0)
+    {
+        output += "Fizz";
+    }
+
+    if (number % 5 == 0)
+    {
+        output += "Buzz";
+    }
+
+    if (output == "")
+    {
+        output = number.ToString();
+    }
+
+    return output;
+}
+
+uint repetitions = 0;
+
+int incorectTries = 0;
+
+Console.WriteLine("Napište počet opakování (kladné číslo): ");
+
+while (!uint.TryParse(Console.ReadLine(), out repetitions) || repetitions == 0)
+{
+    incorectTries++;
+
+    if (incorectTries == 5)
+    {
+        Console.WriteLine("příliš mnoho nesprávných pokusů!");
+        System.Exception ex = new System.Exception("too many tries");
+
+        Console.WriteLine(ex.Message);
+        Environment.Exit(1);
+    }
+
+    Console.WriteLine("Input neodpovídá požadovanému formátu (zkuste to znovu)");
+}
+for (uint i = 1; i <= repetitions; i++)
+{
+    Console.WriteLine(fizzBuzz(i));
+}
