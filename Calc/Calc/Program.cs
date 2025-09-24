57cec497d9e964a28ce193e9cc132f7b0d096890
Ben Michael Dědič <ben.m.dedic@gmail.com>
2025-09-24 09:40:49 +0200

    started working on calculator

---
+++ Calc/Calc/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq.Expressions;
+
+int num1;
+int num2;
+
+string input;
+
+bool validInput = false;
+
+// ToDo: všechno
+
+do
+{
+    Console.Write("zadejte operaci mezi dvěmi čísli s operátory +, -, /, *: ");
+
+    string num;
+    input = 
+    for (int i = 0; i < num.Length; i++)
+    {
+        if input[i]
+    }
+
+}
+while (!validInput);
