9602dfb19719e2ee965c7367ebed0e10db6e6bd1
Ben Michael Dědič <ben.m.dedic@gmail.com>
2025-10-01 09:02:01 +0200

    calculator with equation parsing

--- Calc/Calc/Program.cs
+++ Calc/Calc/Program.cs
@@ -1,24 +1,103 @@
-﻿using System.Linq.Expressions;
-
-int num1;
-int num2;
+﻿using System.Collections.Generic;
 
 string input;
 
 bool validInput = false;
 
+string parsedNum;
+
+string validOperators = "+-/*";
+
+string validNums = "0123456789";
+
+int tmpNum;
+int sum;
+char operand;
+
+LinkedList<string> parsed_input = new LinkedList<string>();
+
 // ToDo: všechno
 
 do
 {
-    Console.Write("zadejte operaci mezi dvěmi čísli s operátory +, -, /, *: ");
+    Console.Write("zadejte operaci mezi celými čísli a operátory +, -, /, *: ");
 
-    string num;
-    input = 
-    for (int i = 0; i < num.Length; i++)
+    parsed_input.Clear();
+
+    input = Console.ReadLine() ?? string.Empty;
+
+    parsedNum = "";
+
+    for (int i = 0; i < input.Length; i++)
     {
-        if input[i]
+        if ((parsedNum == "" && input[i] == '-') || (validNums.Contains(input[i])))
+        {
+            parsedNum += input[i];
+        }
+        else if (validOperators.Contains(input[i]))
+        {
+            if (parsedNum == "")
+            {
+                Console.WriteLine("found operand {0} without number before it at position {1}", input[i], i);
+            }
+
+            if (int.TryParse(parsedNum, out tmpNum))
+            {
+                parsed_input.AddLast(parsedNum);
+                parsed_input.AddLast(input[i].ToString());
+                parsedNum = "";
+            }
+        }
     }
 
+    if (parsed_input.Count() == 0) {
+        Console.WriteLine("couldn't parse equation");
+    }
+    else
+    {
+        if (parsedNum == "" || !(int.TryParse(parsedNum, out tmpNum)))
+        {
+            Console.WriteLine("found trailing operand, ignoring it");
+            parsed_input.RemoveLast();
+        }
+        else
+        {
+            parsed_input.AddLast(parsedNum);
+        }
+
+        int.TryParse(parsed_input.First(), out sum);
+        parsed_input.RemoveFirst();
+        operand = ' ';
+
+        foreach (string element in parsed_input)
+        {
+            if (operand == ' ')
+            {
+                operand = element[0];
+            }
+            else
+            {
+                int.TryParse(element, out tmpNum);
+                switch (operand)
+                {
+                    case '+':
+                        sum += tmpNum;
+                        break;
+                    case '-':
+                        sum -= tmpNum;
+                        break;
+                    case '*':
+                        sum *= tmpNum;
+                        break;
+                    case '/':
+                        sum = sum / tmpNum;
+                        break;
+                }
+                operand = ' ';
+            }
+        }
+        Console.WriteLine("your output is {0}", sum);
+        validInput = true;
+    }
 }
 while (!validInput);
