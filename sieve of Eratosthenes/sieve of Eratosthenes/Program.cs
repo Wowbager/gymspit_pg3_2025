8f4fd5151ed8b1a3ae88acb36fd4cac262434d2a
Ben Michael Dědič <ben.m.dedic@gmail.com>
2025-10-08 09:19:29 +0200

    implementace Eratosthenova síta

---
+++ sieve of Eratosthenes/sieve of Eratosthenes/Program.cs
@@ -0,0 +1,39 @@
+﻿bool valid_input = false;
+uint size;
+
+do
+{
+    Console.WriteLine("Enter uint: ");
+    if (uint.TryParse(Console.ReadLine(), out size))
+    {
+        valid_input = true;
+    }
+} while (!valid_input);
+
+size++;
+
+LinkedList<uint> primes = find_primes(size);
+
+Console.WriteLine($"number of primes {primes.Count}");
+Console.WriteLine($"largest prime: {primes.Last()}");
+
+static LinkedList<uint> find_primes(uint size)
+{
+    LinkedList<uint> list = new LinkedList<uint>();
+
+    bool[] sieve = new bool[size];
+
+    for (uint i = 2; i < size; i++)
+    {
+        if (!sieve[i])
+        {
+            list.AddLast(i);
+            for (uint j = i * i; j < size; j += i)
+            {
+                sieve[j] = true;
+            }
+        }
+    }
+
+    return list;
+}
